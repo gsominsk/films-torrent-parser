12c443bb9794148012e403528cc1fd5ed8c0e501
Georgiy Sominskiy <gsominsk@e3r2p3.unit.ua>
2017-09-14 19:16:15 +0300

    added multiparse function that parsing by many parser-functions

--- libs/parser/index.js
+++ libs/parser/index.js
@@ -100,46 +100,153 @@ function parseYify (start, film, callback) {
 }
 
 function pushToQueue (queue, arr) {
-	for (let i = 0; i < arr.length; i++) {
-		queue.push(arr[i]);
-	}
+    for (let i = 0; i < arr.length; i++) {
+        queue.push(arr[i]);
+    }
 }
 
-module.exports = function (start, film, callback) {
-	parseYify(start, film, function (films) {
-		callback(films);
+class Parsers {
+	constructor () {
+	}
+
+    parseYifyApi (start, film, callback) {
+    	callback([]);
+	}
+
+    parseVifyApi (start, film, callback) {
+    	callback([]);
+	}
+
+    parseYifyWithoutApi (start, film, callback) {
+        let url 	= 'http://yify.is/movie/yifi_filter'+(start==0?'':'/'+start*20)+'?keyword='+encodeURI(film)+'&quality=all&genre=all&rating=0&order_by=latest';
+        let films 	= [];
+        let urls 	= [];
+
+        needle.get(url, (err, res) => {
+            let $ = cheerio.load(res.body);
+            $('.browse-movie-wrap').length == 0 ? callback(urls) : 0;
+
+            let q = async.queue(function (task, callback) {
+                urls[urls.length] = task.children[0].next.attribs.href;
+                callback();
+            }, 20);
+
+            this.pushToQueue(q, $('.browse-movie-wrap'));
+
+            let promise = new Promise((resolve, reject) => {
+                q.drain = function (err) {
+                    if (err) {
+                        reject(err);
+                        return ;
+                    }
+                    resolve(urls);
+                };
+            });
+
+            promise
+                .then(
+                    (result) => {
+                        let qFilmItem = async.queue(function (url, callback) {
+                            needle.get(url, (err, res) => {
+                                let $ = cheerio.load(res.body);
+                                let data = {
+                                    img		: $('.img-responsive')[0].attribs.src,
+                                    name 	: $('#mobile-movie-info')[0].children[1].children[0].data,
+                                    year	: $('#mobile-movie-info')[0].children[3].children[0].data,
+                                    genres	: $('#mobile-movie-info')[0].children[5].children[0].data,
+                                    time	: $('.tech-spec-info')[0].children[3].children[5].children[2].data,
+                                    magnets	: [],
+                                    synopsis: $('#synopsis')[0].children[3].children[0].data,
+                                    imdbUrl	: '',
+                                    cast	: [],
+                                    imdb	: ''
+                                };
+
+                                data.magnets.push($('.magnet-download')[0].attribs.href);
+                                data.magnets.push($('.magnet-download')[1].attribs.href);
+
+                                for (let i = 0; i < $('.list-cast').length; i++) {
+                                    data.cast.push({
+                                        img	: $('.list-cast')[i].children[1].children[1].children[1].attribs.src,
+                                        name: $('.list-cast')[i].children[3].children[1].children[0].data
+                                    });
+                                }
+
+                                for (let i = 0; i < $('.rating-row').length && $('.rating-row')[i].children[1]; i++) {
+                                    if ($('.rating-row')[i].children[1].attribs.title == "IMDb Rating") {
+                                        data.imdbUrl 	= $('.rating-row')[i].children[1].attribs.href;
+                                        data.imdb 		= $('.rating-row')[i].children[3].children[0].data;
+                                        break ;
+                                    }
+                                }
+
+                                films.push(data);
+                                callback();
+                            });
+                        }, 20);
+
+                        qFilmItem.drain = function (err) {
+                            callback(films);
+                        };
+
+                        this.pushToQueue(qFilmItem, result);
+                    },
+                    (error) => {
+                        console.log("Rejected: " + error);
+                    }
+                );
+        });
+	}
+
+	getOwnFunctions () {
+        return (Object.getOwnPropertyNames(Parsers.prototype).filter(function (p) {
+            return (typeof Parsers.prototype[p] === 'function' && p != 'constructor');
+        }));
+	}
+
+    pushToQueue (queue, arr) {
+    	for (let i = 0; i < arr.length; i++) {
+        	queue.push(arr[i]);
+    }
+}
+}
+
+function parse (start, film, func, callback) {
+	let parser 		= new Parsers();
+	let parserFuncs	= parser.getOwnFunctions();
+
+    if (parserFuncs.length <= func + 1) return callback([]);
+
+	parser[parserFuncs[func]](start, film, (films) => {
+		if (films && films.length > 0) {
+			return callback(films);
+		} else {return parse(start, film, ++func, callback)}
 	});
-	// needle.get(url, (err, res) => {
-	// 	// let $ = cheerio.load(res.body);
-	// 	//
-	// 	// let q = async.queue(function(task, callback) {
-	// 	// 	console.log('elo', task.children[0].next.attribs.href);
-	// 	// 	callback();
-	// 	// }, 1);
-	// 	//
-	// 	// q.drain = function() {
-	// 	// 	console.log('all items have been processed');
-	// 	// };
-	// 	//
-	// 	// for (let i = 0; i < $('.browse-movie-wrap').length; i++) {
-	// 	// 	q.push($('.browse-movie-wrap')[i], function(err) {
-	// 	// 		console.log('finished processing item');
-	// 	// 	});
-	// 	// }
-	//
-	// 	// console.log('============================');
-	// 	// console.log($('.browse-movie-wrap')[0]);
-	// 	// console.log('============================'); ready
-	// 	// console.log($('.browse-movie-wrap')[0].children[0].next.attribs.href);
-	// 	// console.log('============================');
-	// 	// console.log($('.browse-movie-wrap')[0].children[2]);
-	// 	// console.log('============================');
-	// 	// console.log($('.browse-movie-wrap')[0].attribs);
-	// 	// console.log('============================');
-	// 	// $('.browse-movie-wrap').each(function () {
-	// 	// 	console.log(this);
-	// 	// });
-	//
-	// 	callback({res: $, url:url})
+}
+
+
+module.exports = function (start, film, callback) {
+
+	parse(start, film, 0, (films) => {
+		callback(films)
+	});
+
+	// let promise = new Promise ((resolve, reject) => {
+    //
 	// });
+
+	// console.log(parser.getFunctions());
+
+	// let parseByApi = new Promise ((resolve, reject) => {
+	// 	parseYifyApi(start, film, (films) => {
+	// 		films && films.length > 0 ?
+	// 	});
+	// });
+
+
+	// parseYify(start, film, function (films) {
+	// 	callback(films);
+	// });
+
+	// callback(['dd']);
 };
